bb1ac4cbdb38e93d968336323a9f8dca04bfd5b3
Will Felix <willfelix93@gmail.com>
2020-07-18 18:10:56 -0300

    Merge

---
+++ MyGames/features/games/collection/GamesCollectionViewController.swift
@@ -0,0 +1,117 @@
+//
+//  GamesCollectionViewController.swift
+//  MyGames
+//
+//  Created by Will Felix on 18/07/20.
+//  Copyright © 2020 Douglas Frari. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+private let reuseIdentifier = "game_collection_cell"
+
+class GamesCollectionViewController: UICollectionViewController {
+    
+    var fetchedResultController: NSFetchedResultsController<Game>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView.register(
+            UINib(nibName: "GameCollectionViewCell", bundle: Bundle.main),
+            forCellWithReuseIdentifier: reuseIdentifier
+        )
+        
+        self.loadGames()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchedResultController?.fetchedObjects?.count ?? 0
+
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! GameCollectionViewCell
+    
+        guard let game = fetchedResultController.fetchedObjects?[indexPath.row] else {
+            return cell
+        }
+        
+        cell.build(with: game)
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+extension GamesCollectionViewController: NSFetchedResultsControllerDelegate {
+    
+    func loadGames(filtering: String = "") {
+        let fetchRequest: NSFetchRequest<Game> = Game.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        if filtering.isEmpty == false {
+            let predicate = NSPredicate(format: "title contains [c] %@", filtering)
+            fetchRequest.predicate = predicate
+        }
+        
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultController.delegate = self
+        
+        do {
+            try fetchedResultController.performFetch()
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
